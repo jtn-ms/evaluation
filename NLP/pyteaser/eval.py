162868829dbba3e2b79187b0aa7abccf2a2d6b75
gustavkkk <gustav0125@outlook.com>
2017-09-12 19:34:13 +0800

    Update eval.py

--- NLP/pyteaser/eval.py
+++ NLP/pyteaser/eval.py
@@ -1,40 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 12 16:02:01 2017
+from unittest import TestCase, main as unittest_main
+from pyteaser import Summarize, SummarizeUrl
 
-@author: Frank
-"""
+class TestSummarize(TestCase):
+    def testText(self):
+        article_title = u'Framework for Partitioning and Execution of Data Stream Applications in Mobile Cloud Computing'
+        article_text = u'The contribution of cloud computing and mobile computing technologies lead to the newly emerging mobile cloud com- puting paradigm. Three major approaches have been pro- posed for mobile cloud applications: 1) extending the access to cloud services to mobile devices; 2) enabling mobile de- vices to work collaboratively as cloud resource providers; 3) augmenting the execution of mobile applications on portable devices using cloud resources. In this paper, we focus on the third approach in supporting mobile data stream applica- tions. More specifically, we study how to optimize the com- putation partitioning of a data stream application between mobile and cloud to achieve maximum speed/throughput in processing the streaming data. To the best of our knowledge, it is the first work to study the partitioning problem for mobile data stream applica- tions, where the optimization is placed on achieving high throughput of processing the streaming data rather than minimizing the makespan of executions as in other appli- cations. We first propose a framework to provide runtime support for the dynamic computation partitioning and exe- cution of the application. Different from existing works, the framework not only allows the dynamic partitioning for a single user but also supports the sharing of computation in- stances among multiple users in the cloud to achieve efficient utilization of the underlying cloud resources. Meanwhile, the framework has better scalability because it is designed on the elastic cloud fabrics. Based on the framework, we design a genetic algorithm for optimal computation parti- tion. Both numerical evaluation and real world experiment have been performed, and the results show that the par- titioned application can achieve at least two times better performance in terms of throughput than the application without partitioning.'
 
-title = "who am I?"
-text = "As a child, I kept thinking and dreaming how I could fly.\
-        Now I am not young, but stilling dreaming about flying.\
-        Some people say I should be realistic.\
-        I also sometimes have to learn how to come along with the mass.\
-        However, pursuit of the great doesn't stop.\
-        I have been kept thinking for it.\
-        I am not sure how it will end.\
-        It is my destiny."
+        summarised_article_text = [u'The contribution of cloud computing and mobile computing technologies lead to the newly emerging mobile cloud com- puting paradigm.', u'Three major approaches have been pro- posed for mobile cloud applications: 1) extending the access to cloud services to mobile devices; 2) enabling mobile de- vices to work collaboratively as cloud resource providers; 3) augmenting the execution of mobile applications on portable devices using cloud resources.', u'In this paper, we focus on the third approach in supporting mobile data stream applica- tions.', u'More specifically, we study how to optimize the com- putation partitioning of a data stream application between mobile and cloud to achieve maximum speed/throughput in processing the streaming data.', u'We first propose a framework to provide runtime support for the dynamic computation partitioning and exe- cution of the application.']
 
-from textblob import TextBlob
-      
-blob = TextBlob(text)
-print(blob.sentences)
+        self.assertEqual(Summarize(article_title, article_text),
+                         summarised_article_text)
+    def testURLs(self):
+        urls = (u'http://www.huffingtonpost.com/2013/11/22/twitter-forward-secrecy_n_4326599.html',
+                u'http://www.bbc.co.uk/news/world-europe-30035666',
+                u'http://www.bbc.co.uk/news/magazine-29631332')
+
+        #just make sure it doesn't crash
+        for url in urls:
+            summaries = SummarizeUrl(url)
 
 
-from gensim.summarization import summarize
-from gensim.summarization import keywords
-           
-print ('Summary:')
-print (summarize(text, ratio=0.5))
-
-print ('Keywords:')
-print (keywords(text, ratio=0.3))
-
-''''''
-from textteaser import TextTeaser
-
-tt = TextTeaser()
-
-sentences = tt.summarize(title, text)
-for sentence in sentences:
-    print(sentence)
+if __name__ == '__main__':
+    unittest_main()
